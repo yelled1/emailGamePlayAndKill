92255e0f54033b3eff5997833854df5d0181bc23
yelled1 <mailhyoon@gmail.com>
2016-10-27 15:46:12 -0400

    Add files via upload

---
+++ RestrictSrc.py
@@ -0,0 +1,7 @@
+SrcDict = { 
+    'imp' : "imap.zoho.com",
+    'usr': 'myGameBoyy@zoho.com',
+    'pwd': 'passme123',
+    'sendr1' : "<restrictiveMom@yahoo.com>",
+    'sendr2' : "<gameCrasherDad@gmail.com>"
+}
